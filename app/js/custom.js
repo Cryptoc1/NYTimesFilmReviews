e51e2e63d8a82256da00c2f13690a9548a60a562
BenP51 <benp51@outlook.com>
2016-09-08 18:59:41 -0700

    Better display, looks more sleek and has buttons! Want to move the buttons but css is a bitch.

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,4 +1,5 @@
 function getInfo(tag,data) {
+  var index = 0;
   var url = "https://api.nytimes.com/svc/movies/v2/reviews/picks.json";
   url += '?' + $.param({
     'api-key': "f40c524ffccf4afd86d8b61f3847250a",
@@ -9,20 +10,20 @@ function getInfo(tag,data) {
     method: 'GET',
   }).done(function(jsonData) {
     if(data == "display_title"){
-      document.getElementById(tag).innerHTML = jsonData.results[0].display_title;
+      document.getElementById(tag).innerHTML = jsonData.results[index].display_title;
     } else if(data == "summary_short") {
-      document.getElementById(tag).innerHTML = jsonData.results[0].summary_short;
+      document.getElementById(tag).innerHTML = jsonData.results[index].summary_short;
     } else if(data == "headline") {
-      document.getElementById(tag).innerHTML = jsonData.results[0].headline;
+      document.getElementById(tag).innerHTML = jsonData.results[index].headline;
     } else if(data == "byline") {
-      document.getElementById(tag).innerHTML = jsonData.results[0].byline;
+      document.getElementById(tag).innerHTML = jsonData.results[index].byline;
     } else if(data == "link") {
-      document.getElementById(tag).innerHTML = jsonData.results[0].link["url"];
-      document.getElementById(tag).setAttribute("href",document.getElementById(tag).innerHTML = jsonData.results[0].link["url"]);
+      document.getElementById(tag).innerHTML = jsonData.results[index].link["url"];
+      //document.getElementById(tag).setAttribute("href",document.getElementById(tag).innerHTML = jsonData.results[index].link["url"]);
     } else if(data == "webview") {
-      document.getElementById(tag).setAttribute("src",jsonData.results[0].link["url"]);
+      document.getElementById(tag).setAttribute("src",jsonData.results[index].link["url"]);
     } else if(data == "img") {
-      document.getElementById(tag).innerHTML = jsonData.results[0].multimedia["src"];
+      document.getElementById(tag).innerHTML = jsonData.results[index].multimedia["src"];
     } else {
       document.getElementById(tag).innerHTML = "Data Error - JSON lookup tag incorrect!";
     }
--- main.js
+++ main.js
@@ -6,7 +6,7 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 800, 'titleBarStyle':'hidden'}) // add 'titleBarStyle': 'hidden' to make title bar go away
+  win = new BrowserWindow({width: 700, height: 800, 'titleBarStyle':'hidden'}) // add 'titleBarStyle': 'hidden' to make title bar go away
 
   // and load the index.html of the app.
   win.loadURL(`file://${__dirname}/app/index.html`)
